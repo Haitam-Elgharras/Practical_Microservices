3b8a73c574710a1cb99f0022e7acfcc8f8a9bd0e
haitam-elgharras <elgharras.haitam@gmail.com>
2024-10-03 15:26:04 +0100

    Advanced approach to create routes in the api-gateway

--- api-gateway/src/main/java/org/glsid/apigateway/ApiGatewayConfig.java
+++ api-gateway/src/main/java/org/glsid/apigateway/ApiGatewayConfig.java
@@ -1,5 +1,8 @@
 package org.glsid.apigateway;
 
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +11,8 @@ import org.springframework.context.annotation.Configuration;
 // this is standard configuration for the gateway we use it only when we have a fixed public API that we want to consume
 @Configuration
 public class ApiGatewayConfig {
-    @Bean
+    // direct access via /products/** or /customers/**
+//    @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
         return builder.routes()
 
@@ -18,6 +22,12 @@ public class ApiGatewayConfig {
                 .route(p -> p.path("/customers/**")
                         .uri("lb://CUSTOMER-SERVICE"))
                 .build();
+    }
+
+    // we must add the service name before the path /CUSTOMER-SERVICE/customers/**
+    @Bean
+    DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties props) {
+        return new DiscoveryClientRouteDefinitionLocator(rdc, props);
     }
 
 }
